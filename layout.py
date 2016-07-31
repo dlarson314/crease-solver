0015e3896e38f246ab74e562560beacc5f6d483f
David Larson <David.Leonard.Larson@gmail.com>
2016-07-30 23:21:20 -0400

    Propagating frames.  New code untested.

--- layout.py
+++ layout.py
@@ -42,7 +42,7 @@ def load_creasepattern(filename):
     return (np.array(node_list), np.array(crease_list), crease_types)
 
 
-def plot_creasepattern(node_list, crease_list, crease_types=None):
+def plot_creasepattern(node_list, crease_list, crease_types=None, triangles=None):
     if crease_types == None:
         crease_types = ['' for i in range(crease_list.shape[0])]
     mpl.figure()
@@ -65,6 +65,12 @@ def plot_creasepattern(node_list, crease_list, crease_types=None):
             mpl.plot(x, y, '--k')
         else:
             mpl.plot(x, y, 'b')
+
+    if triangles is not None:
+        for i in range(triangles.shape[0]):
+            mean_x = np.mean(node_list[triangles[i,:],0])
+            mean_y = np.mean(node_list[triangles[i,:],1])
+            mpl.text(mean_x, mean_y, '%d' % i)
 
     ax.set_aspect('equal')
     mpl.show()
@@ -129,6 +135,9 @@ def solve_triangle_angles(a_deg, b_deg, c_deg):
     """
     Return angles opposites sidea a, b, and c, in a spherical triangle.
     Do internal tests for consistency.
+
+    Later: Choose equations to solve based on numerical accuracy concerns.
+    Don't just use law of cosines, as I do below.
     """
     # Convert to radians
     a = a_deg * np.pi / 180
@@ -471,6 +480,175 @@ crease_angles2[i-1]+---------------+ crease_angles2[i]
             return (crease_angles, opposites)
 
 
+def add_node_creases(known_creases, inode, neighbors, crease_angles):
+    for i in range(len(neighbors)): 
+        known_creases[(inode, neighbors[i])] = crease_angles[i]
+        known_creases[(neighbors[i], inode)] = crease_angles[i]
+    return known_creases
+
+
+def add_flat_creases(known_creases, triangles):
+    for i in range(triangles.shape[0]):
+        edges = [
+            (triangles[i,0], triangles[i,1]),
+            (triangles[i,1], triangles[i,2]),
+            (triangles[i,2], triangles[i,0]),
+            (triangles[i,0], triangles[i,2]),
+            (triangles[i,2], triangles[i,1]),
+            (triangles[i,1], triangles[i,0])]
+        for e in edges:
+            if e not in known_creases:
+                known_creases[e] = 180
+    return known_creases
+
+
+def get_edge2triangle(triangles):
+    """
+    Build a hash that returns the triangle index corresponding to the given
+    edge, with the edge being a pair of node indices.
+    """
+    edge2triangle = {}
+    for i in range(triangles.shape[0]):
+        edges = [
+            (triangles[i,0], triangles[i,1]),
+            (triangles[i,1], triangles[i,2]),
+            (triangles[i,2], triangles[i,0])]
+        for e in edges:
+            edge2triangle[e] = i
+    return edge2triangle
+
+
+def axis_angle_rotation(axis, theta_degrees):
+    """
+    From formula available here:
+    https://en.wikipedia.org/wiki/Rotation_matrix
+    """
+    s = np.sin(theta_degrees * np.pi / 180)
+    c = np.cos(theta_degrees * np.pi / 180)
+    t = 1 - c
+    axis = axis / np.sqrt(np.sum(axis**2))
+    x, y, z = tuple(axis)
+    matrix = np.array(
+        [[c+x**2*t,  x*y*t-z*s, x*z*t+y*s],
+         [x*y*t+z*s,  c+y**2*t, y*z*t-x*s],
+         [z*x*t-y*s, z*y*t+x*s,  c+z**2*t]])
+    return matrix
+
+
+def propagate_frame(nodes, known_creases, edge, frame1, renorm=True):
+    x1 = nodes[edge[0],0] 
+    x2 = nodes[edge[1],0] 
+    y1 = nodes[edge[0],1] 
+    y2 = nodes[edge[1],1] 
+    axis = np.array([x2 - x1, y2 - y1, 0])
+    axis = axis / np.sqrt(np.sum(axis**2))
+    crease_angle = known_creases[edge]
+    matrix = axis_angle_rotation(axis, crease_angle - 180)
+    frame2 = np.dot(np.dot(frame1, matrix), frame1.T)
+    if renorm:
+        # Renormalize the frame2 matrix so it is exactly a rotation matrix
+        # Do this to reduce numerical error?
+        u, s, v = np.linalg.svd(frame2)
+        #print 's = ', s
+        s = np.diag(np.array([1, 1, 1]))
+        frame2 = np.dot(u, np.dot(s, v))
+    return frame2
+
+
+def propagate_frames(nodes, triangles, known_creases, triangle_index=0):
+    edge2triangle = get_edge2triangle(triangles)
+    nt = triangles.shape[0]
+    nn = nodes.shape[0]
+
+    next_triangles = [triangle_index] 
+    frames = [None for i in range(nt)]
+    frames[triangle_index] = np.eye(3)
+    nodes3d = [None for i in range(nn)]
+    # nodes3d for triangle_index remain in the original plane
+    indices = triangles[triangle_index,:]
+    for i in range(3):
+        nodes3d[indices[i]] = np.array([nodes[indices[i],0], nodes[indices[i],1], 0])
+
+    while len(next_triangles) > 0:
+        new_triangles = []
+        for t in next_triangles:
+            edges = [
+                (triangles[t,1], triangles[t,0]),
+                (triangles[t,2], triangles[t,1]),
+                (triangles[t,0], triangles[t,2])]
+            for edge in edges:
+                if edge not in edge2triangle: continue
+                t2 = edge2triangle[edge]
+                if frames[t2] is None:
+                    # Get the frame for the triangle
+                    orig_edge = (edge[1], edge[0])  # Direction in original triangle
+                    frames[t2] = propagate_frame(nodes, known_creases, orig_edge, frames[t], renorm=True)
+                    # Get the location of the new node in the triangle
+                    new_triangles.append(t2)
+                    new_node = list(triangles[t2,:])
+                    new_node.remove(edge[0])
+                    new_node.remove(edge[1])
+                    assert(len(new_node) == 1)
+                    new_node = new_node[0]
+                    x1 = nodes[edge[0],0] 
+                    x2 = nodes[new_node,0] 
+                    y1 = nodes[edge[0],1] 
+                    y2 = nodes[new_node,1] 
+                    vec2d = np.array([x2 - x1, y2 - y1, 0])
+                    vec3d = np.dot(frames[t2], vec2d)
+                    nodes3d[new_node] = nodes3d[edge[0]] + vec3d
+        next_triangles = new_triangles 
+    return frames, nodes3d
+    
+
+class TestFrames(unittest.TestCase):
+    def setUp(self):
+        self.eps = 1e-13 
+        self.nodes = np.array([[0, 0], [1, 0], [0, 1], [0, -1]])
+        self.known_creases = {}
+        self.known_creases[(0,1)] = 270
+        self.known_creases[(1,2)] = 270
+
+    def test1(self):
+        frame1 = np.eye(3)
+        frame2 = propagate_frame(self.nodes, self.known_creases, (0,1), frame1)
+        diff = frame2 - np.array([[1,  0,  0], 
+                                  [0,  0, -1], 
+                                  [0,  1,  0]])
+        self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+        self.known_creases[(0,1)] = 90
+        frame2 = propagate_frame(self.nodes, self.known_creases, (0,1), frame1)
+        diff = frame2 - np.array([[1,  0,  0], 
+                                  [0,  0,  1], 
+                                  [0, -1,  0]])
+        self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+        self.known_creases[(0,1)] = 180
+        frame2 = propagate_frame(self.nodes, self.known_creases, (0,1), frame1)
+        diff = frame2 - np.eye(3)
+        self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+    def test2(self):
+        frame1 = np.eye(3)
+        frame2 = propagate_frame(self.nodes, self.known_creases, (1,2), frame1)
+        diff = np.dot(frame2, frame2.T) - np.eye(3) 
+        self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+        # Pick unusual values for nodes
+        self.nodes = np.array([[0, 0], [0.1, 0.2], [1/np.sqrt(2), 1], [0, -1]])
+        for d in range(0, 360):
+            self.known_creases[(1,2)] = d
+            frame2 = propagate_frame(self.nodes, self.known_creases, (1,2), frame1)
+            diff = np.dot(frame2, frame2.T) - np.eye(3) 
+            self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+            frame2 = propagate_frame(self.nodes, self.known_creases, (1,2),
+                frame1, renorm=False)
+            diff = np.dot(frame2, frame2.T) - np.eye(3) 
+            self.assertTrue(np.amax(np.abs(diff)) < self.eps)
+
+
 def foo():
     node_list, crease_list, crease_types = load_creasepattern('test.creasepattern')
     #print node_list
@@ -490,6 +668,11 @@ def foo():
     crease_angles = [angle, 180, angle, None, angle, 180, angle, None]
     ans = solve_node(neighbor_angles[i], crease_angles)
     print ans
+
+    crease_angles = ans[0]
+    known_creases = {}
+    known_creases = add_node_creases(known_creases, i, neighbors[i], crease_angles)
+    print known_creases
 
     plot_creasepattern(node_list, crease_list, crease_types)
 
@@ -535,7 +718,34 @@ def foo3():
     ax2 = mpl.subplot(122, sharex=ax1, sharey=ax1)
     plot.plot(ax2, **t)
 
-    mpl.show()
+    nodes = t['vertices']
+    triangles = t['triangles']
+    #offset = 0.01
+    for i in range(triangles.shape[0]):
+        mean_x = np.mean(nodes[triangles[i,:],0])
+        mean_y = np.mean(nodes[triangles[i,:],1])
+        mpl.text(mean_x, mean_y, '%d' % i)
+
+    edge2triangle = get_edge2triangle(t['triangles'])
+    print 'edge2triangle'
+    print edge2triangle
+
+    neighbors, neighbor_angles = get_neighbors(node_list, crease_list) 
+    i = 4
+    angle = 15
+    crease_angles = [angle, 180, angle, None, angle, 180, angle, None]
+    ans = solve_node(neighbor_angles[i], crease_angles)
+    crease_angles = ans[0]
+    known_creases = {}
+    known_creases = add_node_creases(known_creases, i, neighbors[i], crease_angles)
+    known_creases = add_flat_creases(known_creases, triangles)
+    print known_creases
+
+    frames, nodes3d = propagate_frames(nodes, triangles, known_creases, triangle_index=0)
+    for i, n in enumerate(nodes3d):
+        print i, n
+
+    plot_creasepattern(nodes, crease_list, crease_types=crease_types, triangles=triangles)
 
 
 if __name__ == "__main__":
